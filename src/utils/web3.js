e720294abe0afa6d84b97af3dfbf737dc9d96a37
jason <kwon_jason@yahoo.com>
2021-03-09 21:51:02 +0300

    project init, add web3 part, prettier config

---
+++ src/utils/web3.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { ethers } from 'ethers'
+
+const connectMetamask = async () => {
+  await window.ethereum.enable()
+  //   handle network change & disconnect here
+  window.ethereum.on('chainChanged', (_chainId) => {
+    //   handle chainId change
+    console.log('chainid is changed to ', _chainId)
+  })
+  window.ethereum.on('disconnect', (error) => {
+    //   handle disconnect
+    console.log('handler for disconnection', error)
+  })
+  window.ethereum.on('accountsChanged', (accounts) => {
+    if (accounts.length == 0) {
+      // handle when no account is connected
+      console.log('disconnected')
+    }
+  })
+  let provider = new ethers.providers.Web3Provider(window.ethereum)
+  let chainId = (await provider.getNetwork()).chainId
+  console.log(`chain id is ${chainId}`)
+  let signer = provider.getSigner()
+  let accounts = await provider.listAccounts()
+  let account = accounts[0]
+  return account, signer
+}
+
+export const Web3Utils = {
+  connectMetamask,
+}
